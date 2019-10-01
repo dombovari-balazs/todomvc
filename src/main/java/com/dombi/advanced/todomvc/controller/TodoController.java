c729af2ad3db353b2f84ecf555f7ad8ce1797039
dombovari-balazs <dbalazs1219@gmail.com>
2019-10-01 22:30:20 +0200

    toggle all, remove by id   roots added

--- src/main/java/com/dombi/advanced/todomvc/controller/TodoController.java
+++ src/main/java/com/dombi/advanced/todomvc/controller/TodoController.java
@@ -3,10 +3,7 @@ package com.dombi.advanced.todomvc.controller;
 import com.dombi.advanced.todomvc.model.Status;
 import com.dombi.advanced.todomvc.model.Todo;
 import com.dombi.advanced.todomvc.repository.TodoDao;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
@@ -33,9 +30,27 @@ public class TodoController {
 
     // Remove all completed
     @DeleteMapping("/todos/completed")
-    public String deleteCompleted(){
+    public String deleteCompleted() {
         TodoDao.removeCompleted();
         return SUCCESS;
     }
+
+    // Toggle all status
+    @PutMapping("/todos/toggle_all")
+    public String toggleAll(@RequestParam HashMap<String,String> map){
+        String complete = map.get("toggle-all");
+        TodoDao.toggleAll(complete.equals("true"));
+        return SUCCESS;
+    }
+
+    // Remove by id
+    @DeleteMapping("/todos/{id}")
+    public String deleteById(@PathVariable String id){
+        TodoDao.remove(id);
+        return SUCCESS;
+    }
+
+
+
 
 }
